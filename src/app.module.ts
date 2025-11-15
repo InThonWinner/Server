b0a75f5acd5618ced2638bacf0ff3fcc1bf6ad91
kminbo <kminbo214@gmail.com>
2025-11-15 16:31:15 +0900

    nestjs Inthon template

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,12 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './health/health.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     AppConfigModule,
     PrismaModule,
     HealthModule,
-    CategoriesModule,
-    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
