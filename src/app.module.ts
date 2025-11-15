732cc99e83f4b181a6115dee587cd4af6c74645f
JaeMin Kim <jeffthehero038@gmail.com>
2025-11-15 23:12:26 +0900

    Unnecessary Folders Deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,19 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
-import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     AppConfigModule,
     PrismaModule,
-    HealthModule,
     AuthModule,
     UserModule,
     PortfolioModule,
-    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
